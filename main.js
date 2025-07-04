ee4d315a4e251ccd461d046ed0f4f214a68ec74e
Gregor Medvesek <medvesek.gregor@gmail.com>
2025-07-04 23:05:41 +0200

    fastify

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ fastify.get("/", async function handler(request, reply) {
 
 // Run the server!
 try {
-  await fastify.listen({ port });
+  await fastify.listen({ port, host: "0.0.0.0" });
 } catch (err) {
   fastify.log.error(err);
   process.exit(1);
