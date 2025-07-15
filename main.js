24cf90d9292f3f3247c79e9b29dd49b140666545
Gregor Medvesek <medvesek.gregor@gmail.com>
2025-07-15 23:02:09 +0200

    donwtime?

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const port = 80;
 
 // Declare a route
 fastify.get("/", async function handler(request, reply) {
-  return { status: "new5" };
+  return { status: "Great success!" };
 });
 
 // Run the server!
