d2789077e92c09db34229679ac85d4e47da27234
Gregor Medvesek <medvesek.gregor@gmail.com>
2025-07-15 22:57:27 +0200

    no downtime

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const port = 80;
 
 // Declare a route
 fastify.get("/", async function handler(request, reply) {
-  return { status: "new3" };
+  return { status: "new4" };
 });
 
 // Run the server!
